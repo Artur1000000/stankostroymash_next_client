a6431bf9d4add010dcc826960774a8390fb0d48e
Artur Arutyunov <aarutyunov852005@gmail.com>
2023-04-13 13:19:48 +0300

    add back paths

--- pages/[category]/[subcategory].js
+++ pages/[category]/[subcategory].js
@@ -1,8 +1,69 @@
+import {
+  Button,
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material";
+import Link from "next/link";
 import { useRouter } from "next/router";
 
-export default function SubCategory() {
+export default function SubCategory({ props }) {
+  console.log(props);
   const router = useRouter();
   console.log(router.query.subcategory, router);
 
-  return <div>1</div>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        gap: "15px",
+        alignItems: "center",
+        paddingTop: "15px",
+        flexFlow: "column wrap",
+      }}
+    >
+      {props.data &&
+        props.data.map((item) => {
+          return (
+            <Card sx={{ maxWidth: 345 }} key={item._id}>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  height="140"
+                  image="https://picsum.photos/id/1019/1000/600/"
+                  alt="green iguana"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {item.title}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Lizards are a widespread group of squamate reptiles, with
+                    over 6,000 species, ranging across all continents except
+                    Antarctica
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                <Link href={`${router.asPath}/${item._id}`}>
+                  <Button size="small" color="primary">
+                    Далее
+                  </Button>
+                </Link>
+              </CardActions>
+            </Card>
+          );
+        })}
+    </div>
+  );
 }
+SubCategory.getInitialProps = async () => {
+  const res = await fetch("https://stankostroymash.onrender.com/api/getall");
+  const data = await res.json().then((res) => res);
+  return {
+    props: { data },
+  };
+};
--- pages/[category]/[subcategory]/[id].js
+++ pages/[category]/[subcategory]/[id].js
@@ -1,3 +1,8 @@
+import {
+  Card,
+  CardContent,
+  Typography,
+} from "@mui/material";
 import { useRouter } from "next/router";
 import ImageGallery from "react-image-gallery";
 import "react-image-gallery/styles/css/image-gallery.css";
@@ -17,20 +22,42 @@ const mockData = [
   },
 ];
 
-export default function SubCategoryId() {
+export default function SubCategoryId({props}) {
+  console.log(props)
   const router = useRouter();
   return (
     <>
-    <h1 style={{textAlign:"center"}}>{router.query.id}</h1>
-      <div style={{width:"600px", height:"400px", margin:"30px auto"}}>
-        <ImageGallery
-          items={mockData}
-          showBullets
-          showPlayButton={false}
-          showIndex
-          showFullscreenButton={false}
-        />
+      <h1 style={{ textAlign: "center" }}>{props.data.subCategory}</h1>
+      <div style={{ width: "600px", height: "400px", margin: "30px auto" }}>
+        <Card
+          variant="outlined"
+          style={{ boxSizing: "border-box", padding: "15px" }}
+        >
+          <ImageGallery
+            items={mockData}
+            showBullets
+            showPlayButton={false}
+            showIndex
+            showFullscreenButton={false}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+            {props.data.title}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Lizards are a widespread group of squamate reptiles, with over
+              6,000 species, ranging across all continents except Antarctica
+            </Typography>
+          </CardContent>
+        </Card>
       </div>
     </>
   );
 }
+SubCategoryId.getInitialProps = async (ctx) => {
+  const res = await fetch(`https://stankostroymash.onrender.com/api/getItem/${ctx.query.id}`);
+  const data = await res.json().then((res) => res);
+  return {
+    props: { data },
+  };
+};
