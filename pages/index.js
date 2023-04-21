46530669779540cf0d872b3c3836beaa2c2d4a66
Artur Arutyunov <aarutyunov852005@gmail.com>
2023-04-21 07:36:06 +0300

    index page

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,7 @@
+import Typography from "@mui/material/Typography";
+import Paper from "@mui/material/Paper";
 import Head from "next/head";
+import { primaryLinks } from "@/components/links";
 
 export default function Index({ props }) {
   console.log(props);
@@ -7,12 +10,38 @@ export default function Index({ props }) {
       <Head>
         <title>СТАНКОСТРОЙМАШ | Главная</title>
       </Head>
-      {/* Главная */}
+      <div
+        style={{
+          display: "flex",
+          width: "100%",
+          gap: "15px",
+          justifyContent: "center",
+          paddingTop: "15px",
+          flexFlow: "column wrap",
+          flexDirection: "row",
+          marginTop: "15px",
+          overflowY: "auto",
+        }}
+      >
+        {primaryLinks.map((item) => {
+          if (item.child) {
+            return item.child.map((i) => {
+              return (
+                <Paper style={{ width: "80%", height: "350px" }} key={i.id}>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {i.title}
+                  </Typography>
+                </Paper>
+              );
+            });
+          }
+        })}
+      </div>
     </>
   );
 }
 Index.getInitialProps = async () => {
-  const res = await fetch("http://localhost:4444/api/getall");
+  const res = await fetch("https://stankostroymash.onrender.com/api/getall");
   const data = await res.json().then((res) => res);
   return {
     props: { main: data },
