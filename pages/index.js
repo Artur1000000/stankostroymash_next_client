86c1ea950899af670bae0463088c20bf44b60127
Artur Arutyunov <aarutyunov852005@gmail.com>
2023-05-02 11:10:02 +0300

    add yandex map

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,8 @@
-import Typography from "@mui/material/Typography";
-import Paper from "@mui/material/Paper";
 import Head from "next/head";
 import { primaryLinks } from "@/components/Menu/links";
 import SubCategoryListBlock from "@/components/SubCategoryListBlock";
 
 export default function Index({ props }) {
-  // console.log(props);
   return (
     <>
       <Head>
@@ -24,30 +21,6 @@ export default function Index({ props }) {
           overflowY: "auto",
         }}
       >
-        {/* {primaryLinks.map((item) => {
-          if (item.child) {
-            return item.child.map((i) => {
-              return (
-                <Paper style={{ width: "80%", height: "350px" }} key={i.id}>
-                  <Typography gutterBottom variant="h5" component="div">
-                    {i.title}
-                  </Typography>
-                  <img src={i.image} alt={i.title} width="150px" height="150" />
-                </Paper>
-              );
-            });
-          }
-          if (item.title === "Запчасти") {
-            return (
-              <Paper style={{ width: "80%", height: "350px" }} key={item.id}>
-                <Typography gutterBottom variant="h5" component="div">
-                  {item.title}
-                </Typography>
-                <img src={item.image} alt={item.title} />
-              </Paper>
-            );
-          }
-        })} */}
         {primaryLinks.map((item) => {
           if (item.child) {
             return <SubCategoryListBlock key={item.id} name={item.title} images={item.child} parent={item.path} />;
